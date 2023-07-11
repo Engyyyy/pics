a7a32bd47eac09aa8306023a46cd374f5a6a6a69
Engy <engeousthepotionmaster@gmail.com>
2023-07-11 18:53:51 +0300

    pass data from child to parent

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,16 @@
-function SearchBar() {
-  return <div>SearchBar</div>;
+function SearchBar({ onSubmit }) {
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(event);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <input />
+      </form>
+    </div>
+  );
 }
 
 export default SearchBar;
