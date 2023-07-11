e6aad7c5854430b2b6213c428323b4f316786f30
Engy <engeousthepotionmaster@gmail.com>
2023-07-11 19:22:20 +0300

    control input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,20 @@
+import { useState } from "react";
+
 function SearchBar({ onSubmit }) {
+  const [term, setTerm] = useState("");
+
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    console.log(event);
+  };
+
+  const handleChange = (event) => {
+    setTerm(event.target.value);
   };
 
   return (
     <div>
       <form onSubmit={handleFormSubmit}>
-        <input />
+        <input value={term} onChange={handleChange} />
       </form>
     </div>
   );
