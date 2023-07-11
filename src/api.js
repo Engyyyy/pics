650fa3c232a0a6398ac0d77b116563b3b82cd5ab
Engy <engeousthepotionmaster@gmail.com>
2023-07-11 17:38:35 +0300

    complete data fetching

--- src/api.js
+++ src/api.js
@@ -1,18 +1,16 @@
 import axios from "axios";
 
-const searchImages = async () => {
+const searchImages = async (searchTerm) => {
   const response = await axios.get("https://api.unsplash.com/search/photos", {
     headers: {
       Authorization: "Client-ID k_dMYIXb3snAhferZKAmKweBfK8x6ntH0fX7_9gXGys",
     },
     params: {
-      query: "oceans",
+      query: searchTerm,
     },
   });
 
-  console.log(response);
-
-  return response;
+  return response.data.results;
 };
 
 export default searchImages;
